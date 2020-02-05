a0ac8de7ae87405321f212edaab0799bac1239ef
Artem Kovera <artemkovera@gmail.com>
2020-02-05 21:17:17 +0200

    added test method and splitting functionality to the constructor

--- linearregression.cpp
+++ linearregression.cpp
@@ -4,7 +4,7 @@
 #include<iostream>
 #include<vector>
 #include<cassert>
-#include<cstdlib>
+#include<cmath>
 
 
 class LinearRegression
@@ -17,13 +17,11 @@ private:
     double meanY;
     double denominator = 0; 
     double numerator = 0;  
-    std::vector<double> mX;
-    std::vector<double> mY;
     std::vector<double> xTrain;
     std::vector<double> yTrain;
     std::vector<double> xTest;
     std::vector<double> yTest;
-    int vectorSize; 
+    bool ifTrained = false;
 
 public:
     
@@ -31,12 +29,37 @@ public:
     {
         //the sizes of input vectors must be equal
         assert(x.size() == y.size());
-    
-        vectorSize = x.size();
-        
 
-       mX = std::move(x);
-       mY = std::move(y);
+        //spliting x input vector into train and test vectors
+        //train=75% and test=25% 
+        for (int i = 0; i<x.size(); i++)
+        {
+            if (i%4 != 0)
+            {   
+                xTrain.push_back(x[i]); 
+            }
+            else
+            {
+                xTest.push_back(x[i]);
+            }
+            
+        } 
+
+        //spliting y input vector into train and test vectors
+        //train=75% and test=25% 
+        for (int i = 0; i<y.size(); i++)
+        {
+            if (i%4 != 0)
+            {   
+                yTrain.push_back(y[i]); 
+            }
+            else
+            {
+                yTest.push_back(y[i]);
+            }
+            
+        } 
+        
     }
 
 //this method applies least squares method for finding cofficients slope and intercept
@@ -44,86 +67,114 @@ public:
     void train () 
     {
         double sumX = 0;
-        for(int i = 0; i<vectorSize; i++)
-        {
-             sumX += mX[i];
-        } 
-        
-        //mean of independent variable x
-        meanX = sumX/vectorSize;
-
         double sumY = 0;
+        int i;
+        int size = xTrain.size();
 
-        for(int i = 0; i<vectorSize; i++)
+        for(i = 0; i<size; i++) 
         {
-             sumY += mY[i];
+           sumX += xTrain[i];     
         } 
-        
+        //mean of independent variable x
+        meanX = sumX/size;
+
+        for(i = 0; i<size; i++)
+        {
+             sumY += yTrain[i];
+        } 
         //mean of dependent variable
-        meanY = sumY/vectorSize; 
+        meanY = sumY/size; 
 
-        // making vector of (x[i] - xMean)
+        // making vector of (xTrain[i] - xMean)
         std::vector<double> vec1;
-        double temp1;
-        for (int i = 0; i < vectorSize; i++) 
+        for (i = 0; i < size; i++) 
         { 
-            temp1 = mX[i] - meanX;
-            vec1.push_back(temp1);
+            vec1.push_back(xTrain[i] - meanX);
         } 
 
-        // making vector of (y[i] - yMean)
+        // making vector of (yTrain[i] - yMean)
         std::vector<double> vec2;   
-        double temp2;
-        for (int i = 0; i < vectorSize; i++) 
+        for (i = 0; i < size; i++) 
         { 
-            temp2 = mY[i] - meanY;
-            vec2.push_back(temp2);
+            vec2.push_back(yTrain[i] - meanY);
         }
         
-        //finding denominator (sum of squares around mean of independent variable)
-        for (int i = 0; i < vectorSize; i++)
+        //finding the denominator (sum of squares around mean of independent variable)
+        for (i = 0; i < size; i++)
         {
-            denominator += vec1[i] * vec1[i];
+            denominator += (vec1[i] * vec1[i]);
         }
 
-        //finding numerator
-        for (int i = 0; i < vectorSize; i++)
+        //finding the numerator
+        for (i = 0; i < size; i++)
         {
             numerator += (vec1[i] * vec2[i]);
         }
 
-        //finding slope
+        //finding the slope
         slope = numerator/denominator;
 
-        //finding intercept
-        intercept = meanY - slope * meanX;
-        
+        //finding the intercept
+        intercept = meanY - slope * meanX;  
+
+        //setting the flag
+        ifTrained = true;
     };
+
+    //method for testing the model
+    //returns Root Mean Square Error (RMSE)
+    double test ()
+    {
+        //this method should be used only if the train method has been used already
+        assert (ifTrained == true);
+
+        double sum = 0;
+        double rmse;
+        int i;
+        int size = xTest.size();
+        std::vector<double> vec;
+        vec.reserve(size);
+
+        for (i = 0; i<size; i++)
+        {
+            vec[i] = predict(xTest[i]);
+        }
+       
+        for (i = 0; i<size; i++)
+        {
+            sum += pow((vec[i] - yTest[i]), 2);
+        }
+        
+        rmse = sum / size; 
+        
+        return rmse; 
+    }
 
     double predict (double independentVal) 
     {
         double predictedValue = intercept + slope * independentVal;
         return predictedValue;
     };
-
+    
 };
 
 int main ()
 {   
     //simulated example where approximately y = 2 + 3 * x  
-    std::vector<double> x {1, 2, 3, 4, 6, 7, 9, 10, 11, 13, 14};
-    std::vector<double> y {5, 7, 11, 14, 20, 24, 29, 32, 34, 41, 45};
+    std::vector<double> x {1, 2, 3, 4, 6, 7, 9, 10, 11, 13, 14, 15, 16, 17, 18, 20, 22, 23, 25};
+    std::vector<double> y {5, 7, 11, 14, 20, 24, 29, 32, 34, 41, 45, 47, 50, 54, 57, 60, 67, 74, 72};
 
-    
-  
     LinearRegression firstLR(x,y);
 
     firstLR.train();
-
+    double testValue = firstLR.test();
+ 
+    std::cout << "Root Mean Square Error is " << testValue << std::endl;
     std::cout << "predicted value for x=5 is " << firstLR.predict(5) << std::endl; // must be close to 17
     std::cout << "predicted value for x=8 is " << firstLR.predict(8) << std::endl; // must be close to 26
     std::cout << "predicted value for x=15 is " << firstLR.predict(15) << std::endl; // must be close to 47
     std::cout << "predicted value for x=0 is " << firstLR.predict(0) << std::endl; // must be close to 2
+    std::cout << "predicted value for x=30 is " << firstLR.predict(30) << std::endl; // must be close to 92
   
     return 0;
 }
